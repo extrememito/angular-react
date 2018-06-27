995f5232bc8d265e965e039982a3c567f81e54f7
Miroslav Gajdos <gjdmiroslav@gmail.com>
2018-06-27 22:13:43 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ReactComponent } from './react/react.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReactComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
